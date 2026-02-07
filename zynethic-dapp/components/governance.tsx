6d555a704d6c78489d43a3f80dffe301335571e1
zynethic <khaqo7890@gmail.com>
2026-02-07 13:57:14 +0700

    Update governance.tsx

--- zynethic-dapp/components/governance.tsx
+++ zynethic-dapp/components/governance.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const triggerSocialPost = async (proposalId: number) => {
   // LAPIS 1: Verifikasi apakah dompet terkoneksi
   if (!isConnected || !walletAddress) {
